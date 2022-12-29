a89ab40c9210b27ad2e36da73cdf1fcb9b7266fb
polskiprogramistanet <rav@polskiprogramista.net>
2022-12-29 17:22:56 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClosuresExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Closures();
+        }
+        static void Closures()
+        {
+            Console.WriteLine(GetFunction()(arg:300));
+        }
+        static Func<int, int> GetFunction()
+        {
+            int val = 100;
+            Func<int, int> internalAdd = x => x + val;
+            Console.WriteLine(internalAdd(arg: 10));
+
+            val = 300;
+            Console.WriteLine(internalAdd(arg:10));
+
+            return internalAdd;
+        }
+    }
+}
